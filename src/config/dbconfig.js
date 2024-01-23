fa9060d7839d5046e8494834eb5eb4669d8fca11
Saurabhabbu-doshaheen <saurabhabbu@doshaheen.com>
2024-01-23 16:56:56 +0530

    included env file

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://saurabhabbu:WZ3UZrEEh3kLt8Cc@sampledb.a4buutr.mongodb.net/?retryWrites=true&w=majority");
+mongoose.connect(process.env.MONGODB_URL);
 
